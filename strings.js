070419de0a716ee75748dc017a0c229c4a5c39c8
Belal Faouri <belalfaourig@gmail.com>
2018-02-25 09:35:06 +0200

    Strings.js Solved

--- strings.js
+++ strings.js
@@ -3,12 +3,24 @@
 // 	Ouput :: "Position of alphabet: 1"
 	function characPosit(character){
 		//your code is here
+		var alph=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+		for (var i = 0; i < alph.length; i++) {
+			if(alph[i]===character){
+				return 'Position of alphabet: ' + (i+1);
+			}
+		}
 	}
 
 // Write a function called repeatStr which repeats the given
 // string string exactly n times.
 // repeatStr("hello",4) ==> 'hellohellohellohello'
-	
+
 	function repeatStr (n, s) {
 		//your code is here
+		var str='';
+		for (var i = 0; i < s; i++) {
+			str+=n;
+		}
+			return str;
 	}
