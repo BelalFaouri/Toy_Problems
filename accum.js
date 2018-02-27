a081e32d70877c6fba376a4909105abfbbd8fddf
Belal Faouri <belalfaourig@gmail.com>
2018-02-27 09:17:23 +0200

    Solved

--- accum.js
+++ accum.js
@@ -4,6 +4,18 @@
 // accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 // accum("cwAt");    // "C-Ww-Aaa-Tttt"
 
+
     function accum(input){
         //your code is here
+        var str='';
+
+        for (var i = 0; i < input.length; i++) {
+          if(i === input.length - 1){
+              str+=input[i].toUpperCase()+(input[i].repeat(i)).toLowerCase();
+          } else{
+              str+=input[i].toUpperCase()+(input[i].repeat(i)).toLowerCase() + '-';
+          }
+
+        }
+        return str;
     }
