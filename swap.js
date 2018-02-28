4204ea24101a4711f8feb141611c0765ea58c765
Belal Faouri <belalfaourig@gmail.com>
2018-02-28 09:19:34 +0200

    swap.js toy problem solved

--- swap.js
+++ swap.js
@@ -3,4 +3,15 @@
 	// ToYPRoblEm --> tOyprOBLeM
 	function swap(input){
 		//your code is here
+		var newStr='';
+
+		for (var i = 0; i < input.length; i++) {
+			if (input[i] === input[i].toUpperCase()) {
+				newStr+=input[i].toLowerCase();
+			} else if (input[i] === input[i].toLowerCase()){
+				newStr+=input[i].toUpperCase();
+
+			}
+		}
+		return newStr;
 	}
