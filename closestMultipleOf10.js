f3b24f26bffc89bacbfb7fadb9a27bb71be8a38b
Belal Faouri <belalfaourig@gmail.com>
2018-03-01 09:21:44 +0200

    last day toy problem

--- closestMultipleOf10.js
+++ closestMultipleOf10.js
@@ -5,4 +5,11 @@
 	// 37 ===> 40
  function closestMultipleOf10(num) {
  	//your code is here
+  for (var i = 1; i < 10; i++) {
+    if(num % 10 === 0 || (num + i) % 10 === 0 ){
+      return num + i;
+    }else if ((num - i) % 10 === 0 ) {
+      return num - i;
+    }
+  }
  }
